fa4e5c0ed507ee51893dbb020ea17b7574d5965b
sorathiya903 <adityasorathiya.4112@gmail.com>
2026-01-03 12:57:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, jsonify 
 from google import genai
+import os
+import webbrowser as wb
 app = Flask(__name__)
-client = genai.Client(api_key="AIzaSyDPXCrN7w-1S8JXgfC2msLuC5twiRRYnsE") 
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 @app.route("/") 
 def home():
     return render_template("index.html") 
 @app.route("/chat", methods=["POST"]) 
 def chat(): 
     user_input = request.json.get("message") 
+    if "open youtube" in user_input:
+     wb.open_new("youtube.com")
+    return
+    if "open google" in user_input:
+     wb.open_new("google.com")
+    
     try: 
         response = client.models.generate_content( model="models/gemini-2.5-flash", contents=user_input ) 
         return jsonify({"reply": response.text}) 
@@ -18,5 +27,6 @@ def chat():
 
 if __name__ == "__main__": 
     app.run()
+
 
 
